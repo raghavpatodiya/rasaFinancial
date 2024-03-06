1e51c1f4881b4fda44df2aa7cf91697be3eba8f0
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-03-06 14:52:28 +0530

    error:stock price retrieval, fix:action calls

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 # commands -
 
-rasa init to initialize
-rasa shell to test in shell
-rasa run to run rasa server
-rasa run --enable-api to enable http server
-rasa run actions to run custom actions defined in actions.py
+# rasa init to initialize
+
+# rasa shell to test in shell
+
+# rasa run to run rasa server
+
+# rasa run --enable-api to enable http server
+
+# rasa run actions to run custom actions defined in actions.py
--- actions/actions.py
+++ actions/actions.py
@@ -5,22 +5,19 @@ import yfinance as yf
 
 class ActionGetStockPrice(Action):
     def name(self) -> Text:
-        return "action_get_stock_price"
+        return "get_stock_price"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
             # Extract the stock name from the user's message
-            stock_name = next(tracker.get_latest_entity_values("stock_name"), None)
+            stock_ticker = next(tracker.get_latest_entity_values("stock_name"), None)
 
-            if stock_name:
+            if stock_ticker:
                 # Fetch stock data from Yahoo Finance
-                stock_data = yf.Ticker(stock_name)
-                
-                # Get current stock price
+                stock_data = yf.Ticker(stock_ticker)
                 current_price = stock_data.history(period='1d')['Close'][0]
-
-                # Respond with the stock price
-                dispatcher.utter_message(text=f"The current stock price of {stock_name} is ${current_price:.2f}")
+                
+                dispatcher.utter_message(text=f"The current stock price of {stock_ticker} is ${current_price:.2f}")
             else:
                 dispatcher.utter_message(text="I couldn't identify the stock name. Please provide a valid stock name.")
         
