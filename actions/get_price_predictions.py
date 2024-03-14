68c60ef652d232b394faa7443e02da00f5b708c8
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-03-14 10:59:44 +0530

    add:requirements.txt

--- actions/get_price_predictions.py
+++ actions/get_price_predictions.py
@@ -6,7 +6,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from actions.ticker_mapping import get_ticker_mapping
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import yfinance as yf
 
 # import os # to get env
@@ -29,11 +29,32 @@ class ActionGetStockPredictions(Action):
     def build_predictive_model(self, df: pd.DataFrame) -> LinearRegression:
         if not df.empty:
             features = ['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits']
-            X = df[features]  # Extracting features
-            y = df['Close']  # Target variable
+            X = df[features]  
+            y = df['Close']  
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)   
             model = LinearRegression()
             model.fit(X_train, y_train)
+
+            # Evaluate model
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+            mse_train = mean_squared_error(y_train, y_pred_train)
+            mse_test = mean_squared_error(y_test, y_pred_test)
+            mae_train = mean_absolute_error(y_train, y_pred_train)
+            mae_test = mean_absolute_error(y_test, y_pred_test)
+            r2_train = r2_score(y_train, y_pred_train)
+            r2_test = r2_score(y_test, y_pred_test)
+
+            # Print evaluation metrics
+            print("Training set evaluation:")
+            print(f"Mean Squared Error (MSE): {mse_train:.2f}")
+            print(f"Mean Absolute Error (MAE): {mae_train:.2f}")
+            print(f"R-squared (R2): {r2_train:.2f}")
+            print("\nTesting set evaluation:")
+            print(f"Mean Squared Error (MSE): {mse_test:.2f}")
+            print(f"Mean Absolute Error (MAE): {mae_test:.2f}")
+            print(f"R-squared (R2): {r2_test:.2f}")
+
             return model, X_test, y_test
             
         else:
