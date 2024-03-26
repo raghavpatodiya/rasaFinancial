8232b00cfc2483b153a95ce16c128b549c6bd8b3
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-03-26 20:41:32 +0530

    fix:comparison

--- actions/comparison.py
+++ actions/comparison.py
@@ -31,12 +31,12 @@ class ActionGetComparison(Action):
                             dispatcher.utter_message(text=f"The current stock prices of {company_name} and {company_name2} are equal (${current_price:.2f}).")
                     else:
                         dispatcher.utter_message(text="Sorry, couldn't retrieve stock price data for one or both of the companies.")
-                # elif info == "market sentiment":
-                #     self.process_market_sentiment(dispatcher, company_name)
-                #     self.process_market_sentiment(dispatcher, company_name2)
-                # elif info == "volatility":
-                #     self.process_stock_volatility(dispatcher, company_name)
-                #     self.process_stock_volatility(dispatcher, company_name2)
+                elif info == "market sentiment":
+                    self.process_market_sentiment(dispatcher, company_name)
+                    self.process_market_sentiment(dispatcher, company_name2)
+                elif info == "volatility":
+                    self.process_stock_volatility(dispatcher, company_name)
+                    self.process_stock_volatility(dispatcher, company_name2)
                 else:
                     dispatcher.utter_message(text="Sorry, I couldn't understand the comparison metric.")
             else:
@@ -62,10 +62,10 @@ class ActionGetComparison(Action):
             dispatcher.utter_message(text="I couldn't identify the stock name. Please provide a valid stock name.")
         pass
 
-    # def process_market_sentiment(self, dispatcher: CollectingDispatcher, company_name: str):
-    #     # Implement the logic to get market sentiment using ActionGetMarketSentiment
-    #     pass
+    def process_market_sentiment(self, dispatcher: CollectingDispatcher, company_name: str):
+        # Implement the logic to get market sentiment using ActionGetMarketSentiment
+        pass
     
-    # def process_stock_volatility(self, dispatcher: CollectingDispatcher, company_name: str):
-    #     # Implement the logic to get stock volatility using ActionGetStockVolatility
-    #     pass
+    def process_stock_volatility(self, dispatcher: CollectingDispatcher, company_name: str):
+        # Implement the logic to get stock volatility using ActionGetStockVolatility
+        pass
