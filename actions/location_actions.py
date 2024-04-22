db065835c0d052a07aff546dd8eed5b63bb1756b
Raghav Patodiya <patodiyaraghav2002@gmail.com>
2024-04-22 11:21:17 +0530

    error:user not authenticated

--- actions/location_actions.py
+++ actions/location_actions.py
@@ -3,7 +3,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from datetime import datetime
 import pytz
-from app import UserLocation
+from app import app, UserLocation
 from flask_login import current_user
 from timezonefinder import TimezoneFinder
 
@@ -13,20 +13,23 @@ class ActionFetchTime(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         user = current_user
-        latitude, longitude = self.get_user_location(user)
-        time = self.fetch_local_time(latitude, longitude)
-        dispatcher.utter_message(f"The current time is: {time}")
-
+        if user and hasattr(user, 'user_id'):
+            latitude, longitude = self.get_user_location(user)
+            if latitude is not None and longitude is not None:
+                time = self.fetch_local_time(latitude, longitude)
+                dispatcher.utter_message(f"The current time is: {time}")
+            else:
+                dispatcher.utter_message("User location not found.")
+        else:
+            dispatcher.utter_message("User not authenticated.")
         return []
 
-    def get_user_location(self, user: int) -> Tuple[float, float]:
-        user_location = UserLocation.query.filter_by(user=user.id).first()
-        if user_location:
-            print(user_location.latitude, user_location.longitude)
-            return user_location.latitude, user_location.longitude
-            
-        else:
-            return 0.0, 0.0
+    def get_user_location(self, user: Any) -> Tuple[float, float]:
+        if user and hasattr(user, 'id'):
+            user_location = UserLocation.query.filter_by(user=user.id).first()
+            if user_location:
+                return user_location.latitude, user_location.longitude
+        return None, None
 
     def fetch_local_time(self, latitude: float, longitude: float) -> str:
         utc_time = datetime.now(pytz.utc)
@@ -42,4 +45,4 @@ class ActionFetchTime(Action):
         if time_zone_str:
             return time_zone_str
         else:
-            return "UTC"  
+            return "UTC"
